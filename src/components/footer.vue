2552ea2a549b6e5e8525e0ce406045b5f174b2f9
vitaly <vitaly.grudachev@servermania.com>
2021-09-30 20:37:47 +0300

    init commit

---
+++ src/components/footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <nav>
+      <a href="#">link</a>
+      <a href="#">link</a>
+      <a href="#">link</a>
+      <a href="#">link</a>
+      <a href="#">link</a>
+      <a href="#">link</a>
+    </nav>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <nav>
+      <a href="#">link</a>
+      <a href="#">link</a>
+      <a href="#">link</a>
+      <a href="#">link</a>
+      <a href="#">link</a>
+      <a href="#">link</a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss"></style>
